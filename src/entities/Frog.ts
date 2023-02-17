51a089162c0ab035924f7aa4c4a11af3e4718b79
Matthew Fotzler <mfotzler@gmail.com>
2023-02-16 22:46:52 -0600

    Add frog and movement on arrow key press

---
+++ src/entities/Frog.ts
@@ -0,0 +1,10 @@
+export class Frog extends Phaser.GameObjects.Sprite {
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, x, y, 'frog');
+        this.setTexture('frog');
+        this.setOrigin(0, 0);
+        this.setScale(1, 1);
+        this.setInteractive();
+        this.scene.add.existing(this);
+    }
+}
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,35 +1,52 @@
 import Phaser from 'phaser';
+import {Frog} from "../entities/Frog";
 
 export default class Demo extends Phaser.Scene {
-  private controls: Phaser.Cameras.Controls.FixedKeyControl | undefined;
-  constructor() {
-    super('GameScene');
-  }
+    private frog?: Frog;
 
-  preload() {
-    this.load.image('tiles', 'assets/tileset.png');
-    this.load.tilemapTiledJSON('map', 'assets/level1.json');
-  }
+    constructor() {
+        super('GameScene');
+    }
 
-  create() {
-    const map = this.make.tilemap({key: 'map'});
-    const tileset = map.addTilesetImage('spritesheet', 'tiles');
-    const layer = map.createLayer(0, tileset, 0, 130);
+    preload() {
+        this.load.image('tiles', 'assets/tileset.png');
+        this.load.tilemapTiledJSON('map', 'assets/level1.json');
+        this.load.image('frog', 'assets/frog.png');
+        this.load.image('car-kun', 'assets/car-kun.png');
+        this.load.image('truck-kun', 'assets/truck-kun.png');
+    }
 
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    create() {
+        const map = this.make.tilemap({key: 'map'});
+        const tileset = map.addTilesetImage('spritesheet', 'tiles');
+        const layer = map.createLayer(0, tileset, 0, 130);
 
-    var cursors = this.input.keyboard.createCursorKeys();
-    var controlConfig = {
-      camera: this.cameras.main,
-      left: cursors.left,
-      right: cursors.right,
-      up: cursors.up,
-      down: cursors.down,
-      speed: 0.5
-    };
-    this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
-  }
-  update(time:number, delta:number) {
-    this.controls?.update(delta);
-  }
+        this.frog = new Frog(this, 0, this.game.config.height as number - 128);
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        this.addFrogInputListeners();
+    }
+
+    update(time: number, delta: number) {
+    }
+
+    addFrogInputListeners() {
+        this.input.keyboard.on('keydown-UP', () => {
+            if (this.frog)
+                this.frog.y -= 128;
+        });
+        this.input.keyboard.on('keydown-DOWN', () => {
+            if (this.frog)
+                this.frog.y += 128;
+        });
+        this.input.keyboard.on('keydown-LEFT', () => {
+            if (this.frog)
+                this.frog.x -= 128;
+        });
+        this.input.keyboard.on('keydown-RIGHT', () => {
+            if (this.frog)
+                this.frog.x += 128;
+        });
+    }
 }
